17f08cfc4250944d15086c8baa5797ea1e2dd8b6
hamishs <hamish.scott@icloud.com>
2024-09-20 17:32:35 +0100

    allow no skip-if

--- src/node_update.rs
+++ src/node_update.rs
@@ -4,19 +4,40 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Default)]
 #[serde(rename_all = "camelCase")]
 pub struct NodeUpdate {
-    #[serde(skip_serializing_if = "Option::is_none")]
+    #[cfg_attr(
+        not(feature = "no-skip-if"),
+        serde(skip_serializing_if = "Option::is_none")
+    )]
     pub label: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
+    #[cfg_attr(
+        not(feature = "no-skip-if"),
+        serde(skip_serializing_if = "Option::is_none")
+    )]
     pub size: Option<f32>,
-    #[serde(skip_serializing_if = "Option::is_none")]
+    #[cfg_attr(
+        not(feature = "no-skip-if"),
+        serde(skip_serializing_if = "Option::is_none")
+    )]
     pub url: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
+    #[cfg_attr(
+        not(feature = "no-skip-if"),
+        serde(skip_serializing_if = "Option::is_none")
+    )]
     pub red: Option<u8>,
-    #[serde(skip_serializing_if = "Option::is_none")]
+    #[cfg_attr(
+        not(feature = "no-skip-if"),
+        serde(skip_serializing_if = "Option::is_none")
+    )]
     pub green: Option<u8>,
-    #[serde(skip_serializing_if = "Option::is_none")]
+    #[cfg_attr(
+        not(feature = "no-skip-if"),
+        serde(skip_serializing_if = "Option::is_none")
+    )]
     pub blue: Option<u8>,
-    #[serde(skip_serializing_if = "Option::is_none")]
+    #[cfg_attr(
+        not(feature = "no-skip-if"),
+        serde(skip_serializing_if = "Option::is_none")
+    )]
     pub show_label: Option<bool>,
 }
 
